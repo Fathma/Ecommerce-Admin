5ded17dfa32450a954a8bd4eaae3d2271cc77e20
Fathma <siddiquefathma@gmail.com>
2019-06-11 14:39:17 +0600

    make it better

--- app.js
+++ app.js
@@ -117,15 +117,15 @@ app.use(function(req, res, next) {
     });
 });
 
-// // Gloabl variables
-// app.use(function(req, res, next) {
-//   res.locals.success_msg = req.flash("success_msg");
-//   res.locals.error_msg = req.flash("error_msg");
-//   res.locals.error = req.flash("error");
-//   res.locals.user = req.user || null;
-//   res.locals.session = req.session;
-//   next();
-// });
+// Gloabl variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
+  res.locals.session = req.session;
+  next();
+});
 
 // middleware
 app.use(async (req, res, next)=>{
